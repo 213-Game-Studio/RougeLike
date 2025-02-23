a5ddbdf72e92d79fe9ddf7facb402e86fa212c4a
Goa1keeper99 <jazzcrocker99@gmail.com>
2025-02-23 18:30:37 -0500

    Set up

---
+++ Scripts/player.gd
@@ -0,0 +1,61 @@
+extends CharacterBody2D
+
+signal player_moved
+
+func _physics_process(delta):
+	player_input()
+
+#Player movement
+func player_input() -> void:
+	if Input.is_action_just_pressed("move_right"):
+		velocity = Vector2.RIGHT
+		move(velocity)
+	elif Input.is_action_just_pressed("move_up"):
+		velocity = Vector2.UP
+		move(velocity)
+	elif Input.is_action_just_pressed("move_left"):
+		velocity = Vector2.LEFT
+		move(velocity)
+	elif Input.is_action_just_pressed("move_down"):
+		velocity = Vector2.DOWN
+		move(velocity)
+	
+	#Player Combat
+	if Input.is_action_just_pressed("attack_right"):
+		try_attack(Vector2.RIGHT)
+	if Input.is_action_just_pressed("attack_left"):
+		try_attack(Vector2.LEFT)
+	if Input.is_action_just_pressed("attack_up"):
+		try_attack(Vector2.UP)
+	if Input.is_action_just_pressed("attacl_down"):
+		try_attack(Vector2.DOWN)
+
+#blocks the player from walking inside the wall
+func move(direction: Vector2) -> void:
+	var space_rid = get_world_2d().space
+	var space_state = PhysicsServer2D.space_get_direct_state(space_rid)
+	var query = PhysicsRayQueryParameters2D.create(global_position, global_position + Vector2(48, 48) * direction)
+	var result = space_state.intersect_ray(query)
+	
+	if result:
+		if result.collider.is_in_group("wall"):
+			return
+	position += 48 * direction
+	
+	player_moved.emit()
+
+func try_attack(direction: Vector2) -> void:
+	var space_rid = get_world_2d().space
+	var space_state = PhysicsServer2D.space_get_direct_state(space_rid)
+	var query = PhysicsRayQueryParameters2D.create(global_position, global_position + Vector2(48, 48) * direction)
+	var result = space_state.intersect_ray(query)
+	
+	if result:
+		if result.collider.is_in_group("Enemy"):
+			result.collider.take_damage(1)
+
+func take_damage(damage : int):
+	Global.health -= damage
+	$AnimationPlayer.play("Hit")
+	if Global.health <= 0:
+		get_tree().reload_current_scene()
